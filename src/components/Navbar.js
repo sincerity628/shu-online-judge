7af77aeeb87bed927805d6b38071ba20ce0bac70
sincerity628 <14785481094@163.com>
2019-12-21 16:38:53 +0800

    fixed navbar error.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,32 +8,22 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <Menu>
-        <Link to="/"><Menu.Item>SHU OJ</Menu.Item></Link>
-        <Link to="/">
-          <Menu.Item name="problem" active={ activeItem ==='problem' }
-            onClick={() => setActiveItem('problem')}>题目
-          </Menu.Item>
-        </Link>
-        <Link to="/status">
-          <Menu.Item name="status" active={ activeItem ==='status' }
-            onClick={() => setActiveItem('status')}>提交状态
-          </Menu.Item>
-        </Link>
-        <Link to="/tests">
-          <Menu.Item name="test" active={ activeItem ==='test' }
-            onClick={() => setActiveItem('test')}>考试
-          </Menu.Item>
-        </Link>
-        <Link to="/groups">
-          <Menu.Item name="group" active={ activeItem ==='group' }
-            onClick={() => setActiveItem('group')}>小组
-          </Menu.Item>
-        </Link>
-        <Link to="/rank">
-          <Menu.Item name="rank" active={ activeItem ==='rank' }
-            onClick={() => setActiveItem('rank')}>排名
-          </Menu.Item>
-        </Link>
+        <Menu.Item as={Link} to="/">SHU OJ</Menu.Item>
+        <Menu.Item name="problem" active={ activeItem ==='problem' }
+          onClick={() => setActiveItem('problem')} as={Link} to="/">题目
+        </Menu.Item>
+        <Menu.Item name="status" active={ activeItem ==='status' }
+          onClick={() => setActiveItem('status')} as={Link} to="/status">提交状态
+        </Menu.Item>
+        <Menu.Item name="test" active={ activeItem ==='test' }
+          onClick={() => setActiveItem('test')} as={Link} to="/tests">考试
+        </Menu.Item>
+        <Menu.Item name="group" active={ activeItem ==='group' }
+          onClick={() => setActiveItem('group')} as={Link} to="/groups">小组
+        </Menu.Item>
+        <Menu.Item name="rank" active={ activeItem ==='rank' }
+          onClick={() => setActiveItem('rank')} as={Link} to="/rank">排名
+        </Menu.Item>
       </Menu>
     </div>
   );
